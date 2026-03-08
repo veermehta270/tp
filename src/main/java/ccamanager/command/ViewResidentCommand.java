cc619b62c89a51cb8e0f03f123e2bb25d3fa5ac8
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2026-03-08 18:06:29 +0800

    feat: add class ViewResidentCommand

---
+++ src/main/java/ccamanager/command/ViewResidentCommand.java
@@ -0,0 +1,17 @@
+package ccamanager.command;
+
+import ccamanager.manager.CcaManager;
+import ccamanager.manager.ResidentManager;
+import ccamanager.ui.Ui;
+
+
+/**
+ * Command to view the list of all the residents
+ */
+public class ViewResidentCommand extends Command {
+
+    @Override
+    public void execute(CcaManager ccaManager, ResidentManager residentManager, Ui ui) {
+
+    }
+}
